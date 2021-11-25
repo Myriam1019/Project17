65963c86a6d79dee61726ef1d57aeb58cc470f9e
15718 <15718@10.0.0.5>
2021-11-24 20:04:30 -0500

    added

--- src/ProjectDemo17/Seventeen.java
+++ src/ProjectDemo17/Seventeen.java
@@ -5,4 +5,13 @@ public class Seventeen {
 	public static void main(String[] args) {
 		System.out.println("Seventeen");
 	}
+	
+	
+	public static void add(int i, int y) {
+		System.out.println(y);
+	}
 }
+	
+
+	
+
